ec723c1eaf9f954701ee098113ce99bad4d153bf
Leo Li <leo.li@adidas.com>
2019-01-09 22:30:24 -0800

    changed webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -13,8 +13,22 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+                  options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+                    '@babel/plugin-proposal-class-properties'
+                    ]
         }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
       }
     ]
   },
